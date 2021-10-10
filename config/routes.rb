455af113caae40d4013b4217f0ce4416c4e49763
Justin Berger Howes <howes.j.j@gmail.com>
2021-10-11 00:11:41 +1100

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/update'
+
+  get 'songs/show'
+
+  get 'songs/index'
+
+  get 'genres/new'
+
+  get 'genres/create'
+
+  get 'genres/update'
+
+  get 'genres/show'
+
+  get 'genres/index'
+
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/update'
+
+  get 'artists/show'
+
+  get 'artists/index'
+
+  resources :songs
+  resources :genres
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
